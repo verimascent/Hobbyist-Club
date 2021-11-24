85b33a06a6d71abceb3e601f338bc3d3d08ef1a1
verimascent <verimascent@gmail.com>
2021-11-24 12:46:46 +0800

    rewrite events controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'events#index'
+  root 'events#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :events
-  match ':controller(/:action(/:id(.:format)))', :via => :all
 end
