913c3c01f4cff150a33718242d5e20922563c8eb
yiwei wang <wywapple@gmail.com>
2021-11-26 13:32:13 +0800

    change seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,14 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 u = User.find(1)
 
-Event.create!(user: u, name: 'Code in the Dark', description: 'Free Flow Pizza', limit:'20', address:'CoderBunder', date: Sat, 04 Dec 2021
-    start_time: 'Sat, 04 Dec 2021 18:00:00.000000000 UTC +00:00', end_time:'Sat, 04 Dec 2021 21:00:00.000000000 UTC +00:00', img_url:'https://media.istockphoto.com/photos/crazy-female-programmer-under-manipulation-in-the-office-picture-id1330154453?b=1&k=20&m=1330154453&s=170667a&w=0&h=jgVnU0qWBp5yfDT8hvFkLUwbEM6nLukJD0y9hmcweKc=')
+Event.create!(user: u, name: 'Code in the Dark', description: 'Free Flow Pizza', limit:20, address:'CoderBunder, Shanghai', date: "Sat, 04 Dec 2021",
+    start_time: "Sat, 04 Dec 2021 18:00:00.000000000 UTC +00:00", end_time: "Sat, 04 Dec 2021 21:00:00.000000000 UTC +00:00", img_url:'https://media.istockphoto.com/photos/crazy-female-programmer-under-manipulation-in-the-office-picture-id1330154453?b=1&k=20&m=1330154453&s=170667a&w=0&h=jgVnU0qWBp5yfDT8hvFkLUwbEM6nLukJD0y9hmcweKc=')
 
-Event.create!(user: u, name: 'Second-hand Swap', description: '冬日断舍离', limit:'15', address:'Yanping Rd', date: Sun, 27 Nov 2021,
-    start_time: 'Sun, 27 Nov 2021 12:30:00.000000000 UTC +00:00', end_time:'Sun, 27 Nov 2021 15:30:00.000000000 UTC +00:00', img_url:'https://media.istockphoto.com/photos/friends-exchanging-presents-at-gettogether-barbecue-party-picture-id1080426408?b=1&k=20&m=1080426408&s=170667a&w=0&h=0viuVXargGR8H4v4xl7e7Mov-ySrUeuBwbvVG_2pZzQ=')
+Event.create!(user: u, name: 'Second-hand Swap', description: '冬日断舍离', limit:15, address:'Yanping Rd, Shanghai', date: "Sun, 27 Nov 2021",
+    start_time: "Sun, 27 Nov 2021 12:30:00.000000000 UTC +00:00", end_time: "Sun, 27 Nov 2021 15:30:00.000000000 UTC +00:00", img_url:'https://media.istockphoto.com/photos/friends-exchanging-presents-at-gettogether-barbecue-party-picture-id1080426408?b=1&k=20&m=1080426408&s=170667a&w=0&h=0viuVXargGR8H4v4xl7e7Mov-ySrUeuBwbvVG_2pZzQ=')
 
-Event.create!(user: u, name: 'Ancient Chinese Book Club', description: 'Read Zuo Zhuan（左传）with Silas', limit:'10', address:'Xnode', date: Sun, 12 Dec 2021
-    start_time: 'Sun, 12 Dec 2021 14:00:00.000000000 UTC +00:00', end_time:'Sun, 12 Dec 2021 16:00:00.000000000 UTC +00:00', img_url:'https://media.istockphoto.com/photos/chinese-traditional-medicine-ancient-book-with-clipping-paths-picture-id92135111?b=1&k=20&m=92135111&s=170667a&w=0&h=ScYyK4yaQcc04E9ghiemoIM1YKRYXxpC09vkJYSn8GQ=')
-    
+Event.create!(user: u, name: 'Ancient Chinese Book Club', description: 'Read Zuo Zhuan（左传）with Silas', limit:10, address:'Xnode, Shanghai', date: "Sun, 12 Dec 2021",
+  start_time: "Sun, 12 Dec 2021 14:00:00.000000000 UTC +00:00", end_time: "Sun, 12 Dec 2021 16:00:00.000000000 UTC +00:00", img_url:'https://media.istockphoto.com/photos/chinese-traditional-medicine-ancient-book-with-clipping-paths-picture-id92135111?b=1&k=20&m=92135111&s=170667a&w=0&h=ScYyK4yaQcc04E9ghiemoIM1YKRYXxpC09vkJYSn8GQ=')
+
+Event.create!(user: u, name: 'Coding with YISY', description: 'This is a codewar between YISY and whoever dare to join', limit:10, address:'BoxingCat, Fuxing Middle Road, Shanghai', date: "Sat, 04 Dec 2021",
+  start_time: "Fri, 26 Nov 2021 19:10:00.000000000 UTC +00:00", end_time: "26 Nov 2021 03:14:08.973980000 UTC +00:00", img_url:'https://i.icanvas.com/ADL9?d=2&sh=v&p=1&bg=g&t=1632145674')
