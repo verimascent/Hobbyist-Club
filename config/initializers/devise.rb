34b7c17651592f694e282141fca531b95600e033
verimascent <verimascent@gmail.com>
2021-11-26 10:35:59 +0800

    mapbox application fix

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -37,6 +37,8 @@ Devise.setup do |config|
   # :mongoid (bson_ext recommended) by default. Other ORMs may be
   # available as additional gems.
   require 'devise/orm/active_record'
+  require 'jquery'
+  require 'jquery_ujs'
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
